0c3168dfb1eb2e6993b4e86fb34b9faa8aa143e7
Dezső Mészáros <meszarosdezsodev@gmail.com>
2020-08-21 10:41:29 +0200

    Create alias.sh

---
+++ alias.sh
@@ -0,0 +1,15 @@
+alias wpw='cd $HOME/Work/Programming/Web'
+alias wpr='cd $HOME/Work/Programming/Rust'
+alias wpf='cd $HOME/Work/Programming/flutter'
+
+alias ot='open -a Terminal `pwd`' # Open terminal from terminal 🤷
+
+alias cna='npx create-next-app --example with-typescript'
+alias dev='npm run dev'
+alias gen='plop'
+
+alias gc='git commit -am'
+alias ga='git add .'
+
+alias bro="brew"
+alias please='sudo'
